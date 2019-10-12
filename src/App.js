529a2de65b30c3de9e26d1a97b94070317c26f64
Ian Liao <ian.liao@verafin.com>
2019-10-12 21:22:42 -0230

    debug

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ class App extends Component {
       }.bind(this),
       error: function(xhr, status, err){
         console.log(err);
-        alert(err);
+        alert(process.env.PUBLIC_URL);
       }
     });
   }
